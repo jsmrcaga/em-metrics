9a5606a9d67c6a73e8b0f4952f4dfd745e023eb2
Jo Colina <jo@jocolina.com>
2025-04-06 23:32:13 +0200

    Pull Requests: simplify signatures - use team_id from db

--- src/models/pull-request.js
+++ src/models/pull-request.js
@@ -6,13 +6,14 @@ const {
 	pull_request_merged_count,
 	pull_request_loc_added,
 	pull_request_loc_removed,
+	pull_request_nb_reviews_per_pr,
 	pull_request_nb_comments_per_review,
 	pull_request_time_to_first_review_minutes,
 	pull_request_time_to_merge_minutes,
 } = require('../metrics/core4/pull-requests');
 
 class PullRequest extends Model {
-	static TABLE_NAME = 'deployments';
+	static TABLE_NAME = 'pull_requests';
 
 	// Deliberately chose not to include project_id
 	// because of the way projects live for teams.
@@ -31,10 +32,12 @@ class PullRequest extends Model {
 		nb_reviews: { type: 'number', default: () => 0 },
 	};
 
-	static created(pr, { additions=null, deletions=null, team_id='unknown' }) {
+	static created(pr, { additions=null, deletions=null }) {
 		if(!(pr instanceof PullRequest)) {
 			throw new Error(`pr must be instance of PullRequest, got ${pr.toString()}`);
 		}
+
+		const { team_id } = pr;
 
 		return PullRequest.objects.insert(pr).then(() => {
 			pull_request_opened_count.increment({
@@ -55,38 +58,54 @@ class PullRequest extends Model {
 		});
 	}
 
-	static reviewed(pr_id, { reviewed_at=new Date().toISOString(), nb_comments=0, team_id='unknown' }) {
+	static reviewed(pr_id, { reviewed_at=new Date().toISOString(), nb_comments=0  }) {
 		return PullRequest.objects.get(pr_id).then(pr => {
+			const { team_id } = pr;
+
 			pull_request_nb_comments_per_review.record(nb_comments, {
 				team_id
 			});
 
 			if(!pr.first_review_at) {
-				pull_request_time_to_first_review_minutes.record(pr.);
+				const time_to_first_review_ms = new Date(reviewed_at).getTime() - new Date(pr.opened_at).getTime();
+				const time_to_first_review_minutes = time_to_first_review_ms / 1000 / 60;
+				pull_request_time_to_first_review_minutes.record(time_to_first_review_minutes, {
+					team_id
+				});
 
 				return PullRequest.objects.update(pr_id, {
-					first_review_at: reviewed_at
+					first_review_at: reviewed_at,
+					nb_reviews: pr.nb_reviews ? pr.nb_reviews + 1 : 1
 				});
 			}
 		});
 	}
 
-	static closed(pr_id, { closed_at=new Date().toISOString(), team_id='unknown' }) {
-		return PullRequest.objects.update(pr_id, {
-			closed_at
-		}).then(() => {
-			pull_request_closed_count.increment({
-				team_id
+	static closed(pr_id, { closed_at=new Date().toISOString() }) {
+		return PullRequest.objects.get(pr_id).then((pr) => {
+			const { team_id } = pr;
+
+			return PullRequest.objects.update(pr_id, {
+				closed_at
+			}).then(() => {
+				pull_request_closed_count.increment({
+					team_id
+				});
 			});
 		});
 	}
 
-	static merged(pr_id, { merged_at=new Date().toISOString(), team_id='unknown', nb_reviews=0 } = {}){
+	static merged(pr_id, { merged_at=new Date().toISOString() } = {}){
 		let time_to_merge_minutes;
+		let nb_reviews;
+		let team_id;
 		return PullRequest.objects.get(pr_id).then(pr => {
 			const time_to_merge_ms = new Date(merged_at).getTime() - new Date(pr.opened_at).getTime();
 			time_to_merge_minutes = time_to_merge_ms / 1000 / 60;
-			
+
+			team_id = pr.team_id;
+			nb_reviews = pr.nb_reviews;
+
 			return PullRequest.objects.update(pr_id, {
 				merged_at
 			});
@@ -96,6 +115,10 @@ class PullRequest extends Model {
 			});
 
 			pull_request_time_to_merge_minutes.record(time_to_merge_minutes, {
+				team_id
+			});
+
+			pull_request_nb_reviews_per_pr.record(nb_reviews, {
 				team_id
 			});
 		});
