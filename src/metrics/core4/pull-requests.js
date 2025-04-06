03af9fd05211d1ab81bf6c4f5785325b7d0d6326
Jo Colina <jo@jocolina.com>
2025-04-06 23:30:45 +0200

    Pull Requests: fix metric types
    Some metrics were being created as Counters but they are Histograms

--- src/metrics/core4/pull-requests.js
+++ src/metrics/core4/pull-requests.js
@@ -13,19 +13,50 @@ const pull_request_merged_count = new Counter('pull_request_merged_count', {
 	unit: 'pull requests'
 });
 
-const pull_request_loc_added = new Counter('pull_request_loc_added', {
-	unit: 'lines of code'
+
+// Histograms
+const LoC_BUCKETS =  [
+	1,
+	10,
+	50,
+	100,
+	300,
+	750,
+	1250,
+	1700,
+	2500,
+	5000
+];
+
+const pull_request_loc_added = new Histogram('pull_request_loc_added', {
+	unit: 'lines of code',
+	advice: {
+		explicitBucketBoundaries: LoC_BUCKETS
+	}
 });
 
-const pull_request_loc_removed = new Counter('pull_request_loc_added', {
-	unit: 'lines of code'
+const pull_request_loc_removed = new Histogram('pull_request_loc_added', {
+	unit: 'lines of code',
+	advice: {
+		explicitBucketBoundaries: LoC_BUCKETS
+	}
 });
 
-const pull_request_nb_comments_per_review = new Counter('pull_request_nb_comments_per_review', {
-	unit: 'comments'
+const pull_request_nb_comments_per_review = new Histogram('pull_request_nb_comments_per_review', {
+	unit: 'comments',
+	advice: {
+		explicitBucketBoundaries: [
+			1,
+			3,
+			5,
+			10,
+			20,
+			30,
+			50
+		]
+	}
 });
 
-// Histogram
 const pull_request_time_to_first_review_minutes = new Histogram('pull_request_time_to_first_review_minutes', {
 	unit: 'minute',
 	advice: {
@@ -60,12 +91,29 @@ const pull_request_time_to_merge_minutes = new Histogram('pull_request_time_to_m
 	}
 });
 
+const pull_request_nb_reviews_per_pr = new Histogram('pull_request_nb_reviews_per_pr', {
+	unit: 'reviews/pr',
+	advice: {
+		explicitBucketBoundaries: [
+			1,
+			2,
+			3,
+			4,
+			5,
+			7,
+			10,
+			15
+		]
+	}
+});
+
 module.exports = {
 	pull_request_opened_count,
 	pull_request_closed_count,
 	pull_request_merged_count,
 	pull_request_loc_added,
 	pull_request_loc_removed,
+	pull_request_nb_reviews_per_pr,
 	pull_request_nb_comments_per_review,
 	pull_request_time_to_first_review_minutes,
 	pull_request_time_to_merge_minutes,
