e3d8836518fa4d3a243f349758a0b04584ff1a79
Jo Colina <jo@jocolina.com>
2025-07-26 08:57:14 +0200

    GitHub Actions - Deployments: parse booleans from env for create_if_not_exists

--- .github/actions/deployments/index.js
+++ .github/actions/deployments/index.js
@@ -1,6 +1,7 @@
 const { EMAPIClient } = require('../lib/em-api-client');
 const { log } = require('../lib/logger');
 const { Process } = require('../lib/process');
+const { EnvParser } = require('../lib/env-parser');
 
 function run() {
 	const em_api_client = new EMAPIClient({
@@ -26,14 +27,16 @@ function run() {
 		});
 	}
 
-	if(!process.env.INPUT_CREATE_IF_NOT_EXISTS || !process.env.INPUT_DEPLOYMENT_START_TIME) {
-		log.error('create_if_not_exists and deployment_start_time are mandatory when type is DEPLOYED');
+	const create_if_not_exists = EnvParser.parse_bool(process.env.INPUT_CREATE_IF_NOT_EXISTS ?? false);
+
+	if(!process.env.INPUT_DEPLOYMENT_START_TIME) {
+		log.error('deployment_start_time is mandatory when type is DEPLOYED');
 		Process.exit(1);
 		return Promise.resolve();
 	}
 
 	return em_api_client.deployments.deployed({
-		create_if_not_exists: Boolean(process.env.INPUT_CREATE_IF_NOT_EXISTS),
+		create_if_not_exists,
 		id: process.env.INPUT_DEPLOYMENT_ID,
 		project_id: process.env.INPUT_PROJECT_ID,
 		first_commit_at: process.env.INPUT_FIRST_COMMIT_AT,
